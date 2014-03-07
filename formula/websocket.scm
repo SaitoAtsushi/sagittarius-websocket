104b7c312db5ef66d617172486e3a5c8c93a393e
SaitoAtsushi <saito@s4.xrea.com>
2014-03-07 22:14:52 +0900

    Added formula file.

---
+++ formula/websocket.scm
@@ -0,0 +1,9 @@
+(formula
+ (description "WebSocket client library")
+ (version "HEAD")
+ (homepage :url "https://github.com/SaitoAtsushi/sagittarius-websocket")
+ (author :name "SAITO Atsushi" :email "saito@s4.xrea.com")
+ (source 
+  :type zip
+  :url "https://github.com/SaitoAtsushi/sagittarius-websocket/archive/master.zip")
+ (install (directories ("src"))))
